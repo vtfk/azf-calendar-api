50026380bafff28728ee49ecb048b3e0f76318fd
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-18 10:13:37 +0100

    Added `tabs` to info log

--- GetMockEventTabs/index.js
+++ GetMockEventTabs/index.js
@@ -2,7 +2,7 @@ const getMockEventTabs = require('../lib/mock/get-mock-event-tabs')
 
 module.exports = async (context, req) => {
   const mockEventTabs = getMockEventTabs(context)
-  context.log(['events', 'get-mock-event-tabs', mockEventTabs.user.userPrincipalName, mockEventTabs.tabs.length])
+  context.log(['events', 'get-mock-event-tabs', mockEventTabs.user.userPrincipalName, 'tabs', mockEventTabs.tabs.length])
 
   return {
     status: 200,

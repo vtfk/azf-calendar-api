339b86c71b0e7273c5685f41ca2ce242b7226733
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-06-01 10:52:57 +0200

    Make sure we have successfully authenticated the user

--- GetMyEventTabs/index.js
+++ GetMyEventTabs/index.js
@@ -1,11 +1,13 @@
 const authGraphUser = require('../lib/auth-graph-user')
 const getUserEventTabs = require('../lib/get-user-event-tabs')
+const HTTPError = require('../lib/http-error')
 
 module.exports = async (context, req) => {
   try {
     const token = req.headers.authorization
     const { force } = req.query
     const graphUser = await authGraphUser(context, req)
+    if (!graphUser || !graphUser.userPrincipalName) throw new HTTPError(403, 'Unauthorized')
     context.log(['events', 'get-my-event-tabs', graphUser.userPrincipalName])
 
     const userEventTabs = await getUserEventTabs(context, token, graphUser, !!force)
